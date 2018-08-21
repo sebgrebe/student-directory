cf3f0509cf1b854fb0775d2c9c0f20fba725c683
Becker <BECKERS2@legal.regn.net>
2018-08-21 14:15:36 +0100

    Exercise 8.1

--- directory.rb
+++ directory.rb
@@ -11,7 +11,23 @@ def input_students
   students
 end
 
-students = input_students
+#fixed set of students
+students_fixed = [
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
+]
+
+# students = input_students
+students = students_fixed
 
 #methods
 def print_header
@@ -19,8 +35,8 @@ def print_header
   puts "-------------"
 end
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, i|
+    puts "#{i+1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 def print_footer(names)
