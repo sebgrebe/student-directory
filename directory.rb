f6c34c431a066fb6470b5202a95b4eb328e4442e
Becker <BECKERS2@legal.regn.net>
2018-08-21 16:17:16 +0100

    exercise 8.12

--- directory.rb
+++ directory.rb
@@ -1,80 +1,54 @@
-#Asking user for student names and cohort
-
+#Asking user for student names
 def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
   students = []
-  name = input_name()
+  name = gets.chomp
   while !name.empty? do
-    cohort = input_cohort
-    if cohort.empty?
-      #set November as default cohort
-      students << {name: name.to_sym, cohort: "November"}
-    else
-      students << {name: name.to_sym, cohort: cohort.to_sym}
-    end
-    name = input_name()
+    students << {name: name, cohort: "November"}
+    name = gets.chomp
   end
   students
-end
-
-def input_name
-  puts "Enter the name of the student (hit return twice to finish)"
-  name = gets[0..-2]
-end
-
-def input_cohort
-  puts "Enter the name of the cohort"
-  cohort = gets[0..-2]
 end
 
 #fixed set of students
 students_fixed = [
   {name: "Dr. Hannibal Lecter", cohort: :november},
   {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :august},
-  {name: "Michael Corleone", cohort: :august},
-  {name: "Alex DeLarge", cohort: :april},
-  {name: "The Wicked Witch of the West", cohort: :april},
-  {name: "Terminator", cohort: :april},
-  {name: "Freddy Krueger", cohort: :december},
-  {name: "The Joker", cohort: :december},
-  {name: "Joffrey Baratheon", cohort: :december},
-  {name: "Norman Bates", cohort: :december}
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
 ]
 
 students = input_students
 # students = students_fixed
-# students = [{name: "Norman Bates", cohort: :december}]
+
 #methods
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
-#Print students grouped by cohort
+#center student name
 def print(students)
-  cohorts = Hash.new
   students.each do |student|
-    if cohorts[student[:cohort]] == nil
-      cohorts[student[:cohort]] = [student[:name]]
-    else
-      cohort = cohorts[student[:cohort]]
-      cohort << student[:name]
-      cohorts[student[:cohort]] = cohort
-    end
-  end
-  cohorts.each do |cohort, students|
-    puts "Students in the #{cohort} cohort:"
-    students.each do |student|
-      puts student
-    end
-    puts "--------------"
+    puts "#{student[:name]}, #{student[:cohort]} cohort".center(45)
   end
 end
 def print_footer(names)
-  students_numerus = (names.count == 1) ? "student" : "students"
-  puts "Overall we have #{names.count} great #{students_numerus}"
+  puts "Overall we have #{names.count} great students".center(45)
 end
 #call methods
-print_header
-print(students)
-print_footer(students)
+if students.count > 0
+  print_header
+  print(students)
+  print_footer(students)
+else
+  nil
+end
